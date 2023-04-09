747e46e551a7fe70fd497736aa60db28dff8a436
Mikhail Pauls <miha.pauls1209@mail.ru>
2023-04-09 19:54:20 +0700

    basic message handlers have been made and interaction with the database has been added

--- code/Telegram.py
+++ code/Telegram.py
@@ -1,47 +1,47 @@
 import telebot
 from telebot import types
-import webbrowser
-import json
-import os
+import sqlite3 as sq
+
 
 # @connect_to_bot.message_handler(commands=['start']) для обработки команд через /
 # @connect_to_bot.message_handler() без параметров принимает любой текст
 
 
-connect_to_bot = telebot.TeleBot('6126143333:AAGOhrq3iXwk4I8y7WOAj6OJdJ0kpiIdAzg')
+with open('telegram_bot_token.txt', 'r') as file:
+    connect_to_bot = telebot.TeleBot(file.readline())
 
 
-def add_and_save_filter_param(chat_id, param):
-    with open('users_data.txt', 'r') as file:
-        if os.stat('users_data.txt').st_size != 0:
-            for_all_users_filter_file = json.load(file)
-        else:
-            for_all_users_filter_file = {}
+def add_new_user_filter_to_db(chat_id, filters):
+    with sq.connect('users_data.db') as connect_to_db:
+        cursor = connect_to_db.cursor()
+        filters_to_db_form = ' '.join(filters)
+        cursor.execute(f"INSERT INTO users_data (chat_id, filters) VALUES ({chat_id}, '{filters_to_db_form}')")
 
-    if chat_id not in for_all_users_filter_file:
-        for_all_users_filter_file[chat_id] = [param]
-    else:
-        if param not in for_all_users_filter_file[chat_id]:
-            for_all_users_filter_file[chat_id] += [param]
 
-    with open('users_data.txt', 'w') as file:
-        json.dump(for_all_users_filter_file, file)
+def update_user_filter_to_db(chat_id, filter):
+    with sq.connect('users_data.db') as connect_to_db:
+        cursor = connect_to_db.cursor()
+        cursor.execute(f"SELECT filters FROM users_data WHERE chat_id = {chat_id}")
+        user_db_filters = cursor.fetchone()[0]
+        user_db_filters_list = user_db_filters.split()
+
+        if filter not in user_db_filters_list:
+            user_db_filters_list.append(filter)
+            filters_to_db_form = ' '.join(user_db_filters_list)
+            cursor.execute(f"UPDATE users_data SET filters = '{filters_to_db_form}' WHERE chat_id = {chat_id}")
 
 
 @connect_to_bot.message_handler(commands=['start'])
 def output_start_message(chat_data):
-    connect_to_bot.send_message(chat_data.chat.id, 'Дороу!')
+    base_filters = ['junior']
+    add_new_user_filter_to_db(chat_data.chat.id, base_filters)
+
+    connect_to_bot.send_message(chat_data.chat.id, 'Здравствуйте!')
 
 
 @connect_to_bot.message_handler(commands=['help'])
 def output_all_command(chat_data):
-    connect_to_bot.send_message(chat_data.chat.id, 'Доступные команды:\n/start\n/help\n/roll\n/setfilter')
-
-
-@connect_to_bot.message_handler(commands=['roll'])
-def output_rickroll(chat_data):
-    connect_to_bot.send_message(chat_data.chat.id, 'Вас зарикроллили!')
-    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    connect_to_bot.send_message(chat_data.chat.id, 'Доступные команды:\n/start\n/help\n/setfilter\n/showfilter')
 
 
 @connect_to_bot.message_handler(commands=['setfilter'])
@@ -61,20 +61,31 @@ def set_filter(chat_data):
 @connect_to_bot.callback_query_handler(func=lambda callback: 'filter_param_' in callback.data)
 def add_filter_param(callback_chat_data):
     filter_param = callback_chat_data.data.split('_')[2]
-    add_and_save_filter_param(str(callback_chat_data.message.chat.id), filter_param)
+    update_user_filter_to_db(callback_chat_data.message.chat.id, filter_param)
 
 
 @connect_to_bot.callback_query_handler(func=lambda callback: callback.data == 'exit')
-def exit_menu_filter(callback_chat_data):
+def exit_filter_menu(callback_chat_data):
     connect_to_bot.delete_message(callback_chat_data.message.chat.id, callback_chat_data.message.message_id)
     connect_to_bot.delete_message(callback_chat_data.message.chat.id, callback_chat_data.message.message_id - 1)
     connect_to_bot.send_message(callback_chat_data.message.chat.id, 'Фильтры установлены!')
 
 
+@connect_to_bot.message_handler(commands=['showfilter'])
+def show_filter(chat_data):
+    with sq.connect('users_data.db') as connect_to_db:
+        cursor = connect_to_db.cursor()
+        cursor.execute(f"SELECT filters FROM users_data WHERE chat_id = {chat_data.chat.id}")
+        user_db_filters = cursor.fetchone()[0]
+        user_db_filters = user_db_filters.replace(' ', ', ')
+
+    connect_to_bot.send_message(chat_data.chat.id, f'Ваши фильтры: {user_db_filters}')
+
+
 @connect_to_bot.message_handler()
 def process_user_message(chat_data):
-    if chat_data.text.lower() == 'ы':
-        connect_to_bot.send_message(chat_data.chat.id, 'А ну вернулся обратно на ветку, бубун!')
+    connect_to_bot.send_message(chat_data.chat.id, 'Пожалуйста введите существующую команду!\n'
+                                                   'Список можно посмотреть с помощью /help')
 
 
 connect_to_bot.infinity_polling()
